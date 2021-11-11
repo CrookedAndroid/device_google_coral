cac8a8c4a2de0e25e5a8b290af26e953cc78aa7e
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    floral: Set TARGET_PREBUILT_KERNEL
    Change-Id: Idf497f71284dc28b2d9f700d5add3b8b1e52f975

--- device-crooked.mk
+++ device-crooked.mk
@@ -10,3 +10,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/coral/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
     device/google/coral/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
+
+TARGET_PREBUILT_KERNEL := device/google/coral-kernel/Image.lz4
